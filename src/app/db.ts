d51c40985a158ca838fd008bed3d676932ebba68
Abhinav Choubey <rockingabhinav2000@gmail.com>
2025-01-04 20:37:40 +0530

    Security: Updated sql configuration

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 export const connection = mysql.createPool({
-	host: "links-manager-links-manager.b.aivencloud.com",
-	user: "avnadmin",
-	password: "AVNS_2HrROsbae8jzr2Jso0f",
-	database: "defaultdb",
-	port: 24604,
+	host: String(process.env.SQL_HOST_NAME),
+	port: Number(process.env.SQL_PORT_NUMBER),
+	user: String(process.env.SQL_USER_NAME),
+	password: String(process.env.SQL_PASSWORD),
+	database: String(process.env.SQL_DATABASE_NAME),
 });
 console.log({
 	host: String(process.env.SQL_HOSTNAME),
