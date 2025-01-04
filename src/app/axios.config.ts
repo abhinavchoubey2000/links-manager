ff13199235a875bd863a0506fba44a92ed7a6733
Abhinav Choubey <rockingabhinav2000@gmail.com>
2025-01-04 21:02:08 +0530

    Fix: Added Vercel URL

--- src/app/axios.config.ts
+++ src/app/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosConfig = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL,
 	headers: {
 		"Content-Type": "application/json",
 	},
